bb18acb87b1a3ef0090346ba265d1c2033b6f4b5
Juan <juanma2002123@gmail.com>
2024-12-14 00:27:01 -0300

    nombrado resources...

--- src/main/java/Controller/Main.java
+++ src/main/java/Controller/Main.java
@@ -10,12 +10,12 @@ import java.net.URL;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        URL fxmlLocation = getClass().getResource("src/main/resources/Views/hello-view.fxml");
+        URL fxmlLocation = Main.class.getResource("newMain.fxml");
         if (fxmlLocation == null) {
-            throw new RuntimeException("No se encontró el archivo hello-view.fxml");
+            throw new RuntimeException("No se encontró el archivo newMain.fxml");
         }
         //FXMLLoader fxmlLoader = new FXMLLoader(fxmlLocation);
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("C:/Users/juan__000/Desktop/Universidad/Pendientes/Software Daniela/Martillero/src/main/resources/Views/hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("newMain.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 300);
         stage.setTitle("JavaFX + Scene Builder");
         stage.setScene(scene);
